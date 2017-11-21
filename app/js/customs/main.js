bb2c1e97ed22a74a7ca2e5d3a5f30d1952165cee
Sergiu <Nikolas.ser.a@yandex.ru>
2017-11-21 17:00:33 +0200

    third on standPC

---
+++ app/js/customs/main.js
@@ -0,0 +1,3 @@
+//= ../../../node_modules/jquery/dist/jquery.js
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+//= my.js
--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,45 @@ var gulp            = require('gulp'),
     sass            = require('gulp-sass'),
     browserSync     = require('browser-sync').create(),
     concat          = require('gulp-concat'),
-    uglify          = require('gulp-uglifyjs');
+    uglify          = require('gulp-uglifyjs'),
+    rigger          = require('gulp-rigger'),
+    sourcemaps       = require('gulp-sourcemaps'),
+    clean           =require('gulp-clean'),
+    rename          =require('gulp-rename'),
+    cssnano         =require('gulp-cssnano');
 
-gulp.task('sass', function () {
+gulp.task('sass', function () {             /*ПРАЦЮЄ*/
     return gulp.src('app/sass/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(cssnano())
+        .pipe(rename({
+            suffix:"-min"
+        }))
+        .pipe(sourcemaps.write("."))
         .pipe(gulp.dest('app/css'))
 });
 
-gulp.task('scriptJS', function () {
-    return gulp.src("node_modules/jquery")
-        .pipe(concat('alljs.js'))
+gulp.task('script', function (){            /*ПРАЦЮЄ*/
+    return gulp.src("app/js/customs/main.js")
+        //.pipe(sourcemaps.init())
+        .pipe(rigger())
         .pipe(uglify())
-        .pipe(gulp.dest("dist/JS"))
+        .pipe(rename({
+             basename: "js",
+             suffix: "-min"
+         }))
+        //.pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("app/js"));
 });
 
 gulp.task('browser-sync', function () {
    browserSync.init({
        server: 'app'
    });
-    gulp.watch('app/sass/*.scss',[sass]);
-    gulp.watch('app/*.html').on('change', browserSync.reload);
+    gulp.watch(['app/sass/*.scss', 'app/*.html'],[sass]).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['sass', 'browser-sync']);
+
+
