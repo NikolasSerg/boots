d44b3b5bb194ca83c09959a2fb11af98e6b90c1e
Sergiu <Nikolas.ser.a@yandex.ru>
2017-12-03 11:04:23 +0200

    repeat on secondPC

--- app/js/customs/main.js
+++ app/js/customs/main.js
@@ -2,5 +2,6 @@
 //= ../../../node_modules/jquery/dist/jquery.js
 //= ../../../node_modules/bootstrap/js/dist/util.js
 //= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/js/dist/carousel.js
 
 //= tooltips.js
