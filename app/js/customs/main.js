8ac0f6220cd961e07dbed5de6ce6938ea1781e5d
Sergiu <Nikolas.ser.a@yandex.ru>
2017-12-10 22:10:02 +0200

    repeat on PC-5

--- app/js/customs/main.js
+++ app/js/customs/main.js
@@ -2,8 +2,10 @@
 //=../../components/popper.js/dist/umd/popper
 
 //= ../../../node_modules/bootstrap/js/dist/util
+// = ../../../node_modules/bootstrap/js/dist/modal
 //= ../../../node_modules/bootstrap/js/dist/tooltip
 //= ../../../node_modules/bootstrap/js/dist/button
 //= ../../../node_modules/bootstrap/js/dist/dropdown
 //= ../../../node_modules/bootstrap/js/dist/carousel
+
 
